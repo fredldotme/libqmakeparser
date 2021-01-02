34e5daf10311fe561d54386762369d4efd09853b
Alfred Neumayer <dev.beidl@gmail.com>
2021-01-03 00:16:49 +0100

    app: Print variables and their values

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -13,5 +13,13 @@ int main(int argc, char** argv)
 	QMakeParser parser(nullptr);
 	parser.setProjectFile(QString::fromUtf8(argv[1], strlen(argv[1])));
 
+	map<QString, QMakeVariable> variables = parser.getVariables();
+	for (auto keyValPair : variables) {
+		cout << "KEY: " << keyValPair.first.toStdString() << endl;
+		for (int i = 0; i < keyValPair.second.values.size(); i++) {
+			cout << "\tValue: " << keyValPair.second.values[i].toStdString() << endl;
+		}
+	}
+
 	return 0;
 }
